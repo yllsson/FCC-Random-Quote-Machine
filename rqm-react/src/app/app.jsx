cb175cb9b3909bd9a200763de22d628d373cb575
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-01-02 17:44:22 +0000

    install react and connect to stylesheet

---
+++ rqm-react/src/app/app.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <main id="quote-box">
+        <article id="text">Insert Random Quote Here</article>
+        <p id="author">author</p>
+        <button id="new-quote">New quote</button>
+        <a href="#" id="tweet-quote">
+          Tweet
+        </a>
+      </main>
+    </div>
+  );
+}
+
+export default App;
