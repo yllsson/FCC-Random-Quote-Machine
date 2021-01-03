3bb02dedf1a59cca996bab0e1a12e4bbf2a11458
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-01-03 16:54:21 +0000

    add skeleton for fetchQuote function

--- rqm-react/src/app/buttons.jsx
+++ rqm-react/src/app/buttons.jsx
@@ -5,10 +5,17 @@ class Buttons extends React.Component {
     super(props);
   }
 
+  // add in function to fetch quotes from quote API on press of new quote button see --> https://forum.freecodecamp.org/t/free-api-inspirational-quotes-json-with-code-examples/311373 and https://type.fit/api/quotes
+  fetchQuote() {
+    //add fetch here
+  }
+
   render() {
     return (
       <div className="buttons">
-        <button id="new-quote">New quote</button>
+        <button onclick={this.fetchQuote()} id="new-quote">
+          New quote
+        </button>
         <a
           href="https://twitter.com/intent/tweet?text=So%20inspiring%21&hashtags=randomquote"
           id="tweet-quote"
