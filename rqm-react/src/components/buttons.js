a91b97913b5379ff295b174dd09dd234fcf39cbd
Ylva-Thorsen <ylva.e.thorsen@gmail.com>
2021-01-18 21:51:01 +0000

    amend link target

--- rqm-react/src/components/buttons.js
+++ rqm-react/src/components/buttons.js
@@ -3,18 +3,14 @@ import React from 'react';
 function Buttons({ quote, updateQuoteBox }) {
   return (
     <div className='buttons'>
-      <button
-        className='themeElement'
-        id='new-quote'
-        target='_blank'
-        onClick={updateQuoteBox}
-      >
+      <button className='themeElement' id='new-quote' onClick={updateQuoteBox}>
         New quote
       </button>
       <a
         className='themeElement'
         href={`https://twitter.com/intent/tweet?text=So%20inspiring%21${` "${quote}"`}&hashtags=randomQuoteMachine%2C100DaysOfCode`}
         id='tweet-quote'
+        target='_blank'
       >
         <i className='fab fa-twitter'></i>
       </a>
