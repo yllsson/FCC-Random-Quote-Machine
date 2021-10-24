88afd7a42eaec970e49156ee82cd28b826a15064
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-10-24 21:48:01 +0100

    Committing comments since it seems my work here is futile

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -8,4 +8,13 @@ describe('App component', () => {
     const loadingMessage = appComponent.getByRole('heading');
     expect(loadingMessage.textContent).toBe('Grabbing Quote...');
   });
+
+  // Seems like I'm wanting to test state when React-Testing-Library seems to think it should not be tested...?
+  // it('should display an error if there is an error', () => {
+  //   const error = `Oh darn, there's an error!`;
+  //   const { getByText } = render(<App error={error} isPending={false} />);
+  //   const header = getByText(/Error:/i);
+  //   console.log(header);
+  //   expect(header.textContent).toBe(`Error: ${error}`);
+  // });
 });
