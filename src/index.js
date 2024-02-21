a93b51e0c5a8da0e7dea52905e200fa8c9ae8ad0
Ylva Turner <ylva.e.thorsen@gmail.com>
2024-02-21 16:20:30 +0000

    Reverting change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './css/style.css';
+import './src/css';
 
 ReactDOM.render(
   <React.StrictMode>
